eb50e269605157f12068ff046d0858763293931e
Zsolt Ádám <zsoltadam92@gmail.com>
2021-03-31 21:05:11 +0200

    add div and js

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const login = document.querySelector("#login");
+
 function myFunction() {
     alert(document.getElementById('userName').value);
-  }
+}
+  
+login.addEventListener('click', myFunction);
